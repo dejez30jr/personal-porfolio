df0ea5f7514f036cec3e07462fd56f8e44fe6ca7
deris <165728707+dejez30jr@users.noreply.github.com>
2024-11-02 23:30:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,46 @@ window.onscroll=  function(){
         nav.style.background= "transparent"
     }
 }
+
+// popup open
+const kontak = document.querySelectorAll('.kontak');
+kontak.forEach(card=>{
+    card.addEventListener('click', function(){
+        const popup= this.querySelector('.popup');
+        popup.style.display= "flex";
+    })
+})
+const project = document.querySelectorAll('.icon-project');
+project.forEach(card=>{
+    card.addEventListener('click', function(){
+        const popup= this.querySelector('.popup');
+        popup.style.display= "flex";
+    })
+})
+
+
+const closeButtons = document.querySelectorAll('.close');
+closeButtons.forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.stopPropagation(); 
+        const popup = this.closest('.popup');
+        popup.style.display = 'none';
+    });
+});
+
+ // Mencegah shortcut keyboard tertentu (misalnya Ctrl+S, Ctrl+P, dll)
+ document.addEventListener('keydown', function(e) {
+    // Mencegah kombinasi Ctrl + S (Save)
+    if (e.ctrlKey && e.key === 's') {
+        e.preventDefault();
+    }
+    // Mencegah kombinasi Ctrl + P (Print)
+    if (e.ctrlKey && e.key === 'p') {
+        e.preventDefault();
+    }
+    // Mencegah kombinasi Ctrl + U (View source code)
+    if (e.ctrlKey && e.key === 'u') {
+        e.preventDefault();
+    }
+    // Tambahkan kombinasi shortcut lain yang ingin dicegah
+});
